afcf67104974452a9a5715b4cbd973cefae50462
garchomp-game <otoka.japanesedrum@gmail.com>
2017-12-02 18:11:21 +0900

    facebook_twitter_メアド認証機能完成！

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root 'home#index', to: "home#index", as: "user_root"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
